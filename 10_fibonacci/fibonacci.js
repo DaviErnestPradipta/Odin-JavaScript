ed3ba52c9d0b715f2a965304cfcd11414f4d070f
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-23 19:54:26 +0900

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    const fib = [0, 1];
+    for (let i = 2; i < number; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[number]
 };
 
 // Do not edit below this line
