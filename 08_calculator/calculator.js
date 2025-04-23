e0fe816f30ac110d6cc27f63b2cd58cbcca7ab3b
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-23 19:46:04 +0900

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(numberOne, numberTwo) {
+  return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberOne, numberTwo) {
+	return numberOne - numberTwo
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let arraySum = 0;
+  for (let i = 0; i <= array.length; i++) arraySum += array[i];
+  return arraySum;	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let arrayProduct = 1;
+  if (array.length === 0) return 0;
+  else for (let i = 0; i <= array.length; i++) arrayProduct *= array[i];
+  return arrayProduct;	
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let result = 1;
+  for (let i = 0; i <= exponent; i++) result *= base;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for (let i = number; i > 0; i--) result *= i;
+  return result
 };
 
 // Do not edit below this line
