26fea1daf89a2d7677530e53ffde7a3ae28f0d76
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-23 19:35:00 +0900

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4;
+    const isDivisibleByOneHundred = year % 100;
+    const isDivisibleByFourHundred = year % 400;
 
+    if (isDivisibleByFour && 
+        (!isDivisibleByOneHundred || isDivisibleByFourHundred)) 
+        return true;
+    else return false;
 };
 
 // Do not edit below this line
