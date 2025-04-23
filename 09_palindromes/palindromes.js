356386053f78ffbd75f386038b830ebd503a256f
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-23 19:50:45 +0900

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let j = string.length - 1;
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] != string[j]) return false;
+        j--;
+    }
+    return true
 };
 
 // Do not edit below this line
