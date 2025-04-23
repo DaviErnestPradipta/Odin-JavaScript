0fb42d11431fd4f4538fc8fc18f3231f1f62ab1c
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-23 19:20:07 +0900

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (string === "")
+        return "";
+    else
+        return reverseString(string.substr(1)) + string.charAt(0);
 };
 
 // Do not edit below this line
