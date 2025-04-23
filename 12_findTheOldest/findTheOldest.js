4808477c5306126aad3c039be5c2c42b1e33a41d
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-23 20:00:48 +0900

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const getAge = function(person) {
+    person.yearOfDeath ??= new Date().getFullYear();
 };
+
+const findTheOldest = function(person) {
+    const peopleOldestToYoungest = people.toSorted(
+        (person, nextPerson) => getAge(nextPerson) - getAge(person),
+    );
+
+    const oldestPerson = peopleOldestToYoungest[0];
+    return oldestPerson;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
